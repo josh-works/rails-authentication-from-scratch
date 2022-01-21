ea5ba3707945a7b1e31f95cc04f4265bb56271bb
Josh Thompson <thompsonjoshd@gmail.com>
2022-01-21 13:33:19 -0700

    Finish step 3

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  root 'static_pages#home'
+  post "sign_up", to: "users#create"
+  get "sign_up", to: "users#new"
+  
+  
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
